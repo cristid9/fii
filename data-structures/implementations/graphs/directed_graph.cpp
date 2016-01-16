faa28ec4762bdef1b506a00ed4725fb2c89c0a00
Cristi D <cristi.dnla@gmail.com>
2016-01-16 18:46:41 +0200

    Add insertArc

--- data-structures/implementations/graphs/directed_graph.cpp
+++ data-structures/implementations/graphs/directed_graph.cpp
@@ -1,7 +1,23 @@
 #include "directed_graph.h"
+#include <vector>
+#include <algorithm>
+
+using namespace std;
 
 Digraph::Digraph(int v)
 {
+    nodes = v;
 
+    for (int i = 0; i < v; i++)
+        edges.push_back(vector<int>());
 }
+
+void Digraph::insertArc(int v, int w)
+{
+    if (find(edges.begin(), edges.end(), v) != edges.end())
+        return;
+
+    edges[v].push_back(w);
+}
+
 
