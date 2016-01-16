d0101735ae08f184f08a772c4f57ba90e657b029
Cristi D <cristi.dnla@gmail.com>
2016-01-16 20:24:17 +0200

    Complete the implementation

--- data-structures/implementations/graphs/directed_graph.cpp
+++ data-structures/implementations/graphs/directed_graph.cpp
@@ -20,26 +20,53 @@ void Digraph::insertArc(int v, int w)
     edges[v].push_back(w);
 }
 
-void Graph::removeArc(int v, int w)
+void Digraph::removeArc(int v, int w)
 {
-    if (find(edges.begin(), edges.end(), w) == edges.end())
+    if (find(edges[v].begin(), edges[v].end(), w) == edges[v].end())
         return;
 
     edges[v].erase(remove(edges[v].begin(), edges[v].end(), w));
 }
 
-vector<int> Graph::adj(int v)
+vector<int> Digraph::internAdj(int v)
 {
     return edges[v];
+}
+
+vector<int> Digraph::externAdj(int v)
+{
+    return transpose().internAdj(v);
+}
+
+vector<int> Digraph::accessibleFrom(int v)
+{
+    vector<int> neighbors;
+    visit(v, neighbors);
+
+    neighbors.erase(remove(neighbors.begin(), neighbors.end(), v));
+
+    return neighbors;
+}
+
+void Digraph::visit(int v, vector<int>& neighbors)
+{
+    if (find(neighbors.begin(), neighbors.end(), v) != neighbors.end())
+        return;
+
+    vector<int> tmp = internAdj(v);
+    neighbors.push_back(v);
+
+    for (auto it = tmp.begin(); it != tmp.end(); it++)
+        visit(*it, neighbors);
 }
 
 Digraph Digraph::transpose()
 {
     Digraph t(nodes);
 
-    for (int vertex = 0; i < nodes; vertex++)
+    for (int vertex = 0; vertex < nodes; vertex++)
     {
-        vector<int> tmp = adj(i);
+        vector<int> tmp = internAdj(vertex);
         for (auto it = tmp.begin(); it != tmp.end(); it++)
         {
             insertArc(*it, vertex);
